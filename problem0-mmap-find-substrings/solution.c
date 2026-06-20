265c830ad57ef770dd8384f86c6bde9d1bc36b28
Мария Позднякова <pozdnyakova.mary@inbox.ru>
2026-06-20 19:27:55 +0300

    Update solution.c

--- problem0-mmap-find-substrings/solution.c
+++ problem0-mmap-find-substrings/solution.c
@@ -18,9 +18,49 @@ int main(int argc, char *argv[]) {
         return 1;
     }
 
-    // TODO: откройте файл, получите его размер через fstat,
-    //       отобразите в память через mmap,
-    //       найдите все вхождения argv[2] и выведите их позиции
+    const char *filename = argv[1];
+    const char *search_str = argv[2];
+    size_t search_len = strlen(search_str);
+
+    if (search_len == 0) {
+        return 0;
+    }
+
+    int fd = open(filename, O_RDONLY);
+    if (fd < 0) {
+        perror("open");
+        return 1;
+    }
+
+    struct stat st;
+    if (fstat(fd, &st) < 0) {
+        perror("fstat");
+        close(fd);
+        return 1;
+    }
+
+    if (st.st_size == 0 || (size_t)st.st_size < search_len) {
+        close(fd);
+        return 0;
+    }
+
+    char *map = mmap(NULL, st.st_size, PROT_READ, MAP_PRIVATE, fd, 0);
+    if (map == MAP_FAILED) {
+        perror("mmap");
+        close(fd);
+        return 1;
+    }
+
+    for (size_t i = 0; i <= (size_t)st.st_size - search_len; ++i) {
+        if (memcmp(map + i, search_str, search_len) == 0) {
+            printf("%zu\n", i);
+        }
+    }
+
+    if (munmap(map, st.st_size) < 0) {
+        perror("munmap");
+    }
+    close(fd);
 
     return 0;
 }
